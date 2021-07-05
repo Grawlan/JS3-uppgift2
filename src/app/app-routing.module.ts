5c228362a0a5463ee717ba5081ecd5134b003f9a
Grawlan <izikiel@gmail.com>
2021-07-05 09:41:53 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EducationComponent } from './views/education/education.component';
+import { ExperienceComponent } from './views/experience/experience.component';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'experience', component: ExperienceComponent},
+  { path: 'education', component: EducationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { EducationComponent } from './views/education/education.component';
+import { ExperienceComponent } from './views/experience/experience.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    EducationComponent,
+    ExperienceComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
